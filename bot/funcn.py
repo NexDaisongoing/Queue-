e95370aaef2f33b15eb5b024e68188c6e1093cdc
NexDaisongoing <125869769+NexDaisongoing@users.noreply.github.com>
2024-11-05 19:09:51 +0530

    Add files via upload

---
+++ bot/funcn.py
@@ -0,0 +1,243 @@
+#    This file is part of the CompressorQueue distribution.
+#    Copyright (c) 2021 Danish_00
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+# License can be found in <
+# https://github.com/1Danish-00/CompressorQueue/blob/main/License> .
+
+from . import *
+from .config import *
+
+WORKING = []
+QUEUE = {}
+OK = {}
+
+uptime = dt.now()
+os.system(f"wget {THUMB} -O thumb.jpg")
+
+if not os.path.isdir("downloads/"):
+    os.mkdir("downloads/")
+if not os.path.isdir("encode/"):
+    os.mkdir("encode/")
+if not os.path.isdir("thumb/"):
+    os.mkdir("thumb/")
+
+
+tgp_client = TelegraphPoster(use_api=True, telegraph_api_url=TELEGRAPH_API)
+
+
+def create_api_token():
+    retries = 10
+    telgrph_tkn_err_msg = (
+        "Couldn't not successfully create api token required by telegraph to work"
+        "\nAs such telegraph is therefore disabled!"
+    )
+    while retries:
+        try:
+            tgp_client.create_api_token("Mediainfo")
+            break
+        except (requests.exceptions.ConnectionError, ConnectionError) as e:
+            retries -= 1
+            if not retries:
+                LOGS.info(telgrph_tkn_err_msg)
+                break
+            time.sleep(1)
+
+
+create_api_token()
+
+
+def stdr(seconds: int) -> str:
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    if len(str(minutes)) == 1:
+        minutes = "0" + str(minutes)
+    if len(str(hours)) == 1:
+        hours = "0" + str(hours)
+    if len(str(seconds)) == 1:
+        seconds = "0" + str(seconds)
+    dur = (
+        ((str(hours) + ":") if hours else "00:")
+        + ((str(minutes) + ":") if minutes else "00:")
+        + ((str(seconds)) if seconds else "")
+    )
+    return dur
+
+
+def ts(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        ((str(days) + "d, ") if days else "")
+        + ((str(hours) + "h, ") if hours else "")
+        + ((str(minutes) + "m, ") if minutes else "")
+        + ((str(seconds) + "s, ") if seconds else "")
+        + ((str(milliseconds) + "ms, ") if milliseconds else "")
+    )
+    return tmp[:-2]
+
+
+def hbs(size):
+    if not size:
+        return ""
+    power = 2**10
+    raised_to_pow = 0
+    dict_power_n = {0: "B", 1: "K", 2: "M", 3: "G", 4: "T", 5: "P"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
+No_Flood = {}
+
+
+async def progress(current, total, event, start, type_of_ps, file=None):
+    now = time.time()
+    if No_Flood.get(event.chat_id):
+        if No_Flood[event.chat_id].get(event.id):
+            if (now - No_Flood[event.chat_id][event.id]) < 1.1:
+                return
+        else:
+            No_Flood[event.chat_id].update({event.id: now})
+    else:
+        No_Flood.update({event.chat_id: {event.id: now}})
+    diff = time.time() - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        time_to_completion = round((total - current) / speed) * 1000
+        progress_str = "`[{0}{1}] {2}%`\n\n".format(
+            "".join(["●" for i in range(math.floor(percentage / 5))]),
+            "".join(["○" for i in range(20 - math.floor(percentage / 5))]),
+            round(percentage, 2),
+        )
+        tmp = (
+            progress_str
+            + "`{0} of {1}`\n\n`✦ Speed: {2}/s`\n\n`✦ ETA: {3}`\n\n".format(
+                hbs(current),
+                hbs(total),
+                hbs(speed),
+                ts(time_to_completion),
+            )
+        )
+        if file:
+            await event.edit(
+                "`✦ {}`\n\n`File Name: {}`\n\n{}".format(type_of_ps, file, tmp)
+            )
+        else:
+            await event.edit("`✦ {}`\n\n{}".format(type_of_ps, tmp))
+
+
+async def info(file, event=None):
+    # depreciating second arg.
+    try:
+        author = (await bot.get_me()).first_name
+        author_url = f"https://t.me/{((await bot.get_me()).username)}"
+        out = pymediainfo.MediaInfo.parse(file, output="HTML", full=False)
+        if len(out) > 65536:
+            out = (
+                out[:65430]
+                + "<strong>...<strong><br><br><strong>(TRUNCATED DUE TO CONTENT EXCEEDING MAX LENGTH)<strong>"
+            )
+        retries = 10
+        while retries:
+            try:
+                page = tgp_client.post(
+                    title="Mediainfo",
+                    author=author,
+                    author_url=author_url,
+                    text=out,
+                )
+                break
+            except (requests.exceptions.ConnectionError, ConnectionError) as e:
+                retries -= 1
+                if not retries:
+                    raise e
+                await asyncio.sleep(1)
+        return page["url"]
+    except Exception:
+        return None
+
+
+def code(data):
+    OK.update({len(OK): data})
+    return str(len(OK) - 1)
+
+
+def decode(key):
+    if OK.get(int(key)):
+        return OK[int(key)]
+    return
+
+
+async def skip(e):
+    wah = e.pattern_match.group(1).decode("UTF-8")
+    wh = decode(wah)
+    out, dl, id = wh.split(";")
+    try:
+        if QUEUE.get(int(id)):
+            WORKING.clear()
+            QUEUE.pop(int(id))
+        await e.delete()
+        os.remove(dl)
+        os.remove(out)
+        # Lets kill ffmpeg else it will run in memory even after deleting
+        # input.
+        for proc in psutil.process_iter():
+            processName = proc.name()
+            processID = proc.pid
+            print(processName, " - ", processID)
+            if processName == "ffmpeg":
+                os.kill(processID, signal.SIGKILL)
+    except BaseException:
+        pass
+    return
+
+
+async def fast_download(e, download_url, filename=None):
+    def progress_callback(d, t):
+        return (
+            asyncio.get_event_loop().create_task(
+                progress(
+                    d,
+                    t,
+                    e,
+                    time.time(),
+                    f"Downloading from {download_url}",
+                )
+            ),
+        )
+
+    async def _maybe_await(value):
+        if inspect.isawaitable(value):
+            return await value
+        else:
+            return value
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(download_url, timeout=None) as response:
+            if not filename:
+                filename = download_url.rpartition("/")[-1]
+            filename = os.path.join("downloads", filename)
+            total_size = int(response.headers.get("content-length", 0)) or None
+            downloaded_size = 0
+            with open(filename, "wb") as f:
+                async for chunk in response.content.iter_chunked(1024):
+                    if chunk:
+                        f.write(chunk)
+                        downloaded_size += len(chunk)
+                        await _maybe_await(
+                            progress_callback(downloaded_size, total_size)
+                        )
+            return filename
