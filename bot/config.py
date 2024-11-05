eed491e0bdf20f344842b34f290db3716b557d62
NexDaisongoing <125869769+NexDaisongoing@users.noreply.github.com>
2024-11-05 19:12:57 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,11 +16,11 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", default=6, cast=int)
-    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    BOT_TOKEN = config("BOT_TOKEN")
-    DEV = 1322549723
-    OWNER = config("OWNER")
+    APP_ID = config("10419615", default=6, cast=int)
+    API_HASH = config("412bca750b8004784adeeafb98e991c5", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    BOT_TOKEN = config("7882507899:AAHVLoQJ7mCnihdxlMTiI0LkMV7cmbXd5no")
+    DEV = 7077099034
+    OWNER = config("7077099034")
     FFMPEG = config(
         "FFMPEG",
         default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
